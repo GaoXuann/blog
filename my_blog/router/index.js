42ab9c38b853b4bcdc3bfef81b29715f1d029bb9
gaoxuan <gaoxuan961217@sina.com>
2018-11-06 12:50:08 +0800

    完成模块抽离

--- my_blog/router/index.js
+++ my_blog/router/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-router.get('/', (req, res) => {
-    res.render('index.ejs', {})
-})
+const ctrl = require('../controller/index.js')
+router.get('/', ctrl.showIndex)
 module.exports = router
