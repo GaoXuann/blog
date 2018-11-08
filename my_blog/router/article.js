cf7128c52149edabaee09d9ea2b1599aeafde309
gaoxuan <gaoxuan961217@sina.com>
2018-11-08 20:59:25 +0800

    文章详情页添加查看完成

--- my_blog/router/article.js
+++ my_blog/router/article.js
@@ -2,5 +2,7 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../controller/article.js')
 
-router.get('/article/add', ctrl.add)
+router.get('/article/add', ctrl.showAdd)
+router.post('/article/add', ctrl.add)
+router.get('/article/info/:id', ctrl.info)
 module.exports = router
