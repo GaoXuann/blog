bf95aa763a5f22de13ad8fed6ac153064fefcbd4
gaoxuan <gaoxuan961217@sina.com>
2018-11-09 21:57:12 +0800

    主页面渲染文章完成

--- my_blog/controller/index.js
+++ my_blog/controller/index.js
@@ -1,14 +1,27 @@
 const conn = require('../db/db.js')
 module.exports = {
     showIndex: (req, res) => {
-        const sql = "select * from article"
+        const sql = `select a.id, a.title, a.c_time,u.nickname 
+         from article as a 
+         LEFT JOIN user as u 
+         on a.author_id = u.id 
+         ORDER BY a.id desc`;
         conn.query(sql, (err, data) => {
+            if (err) {
+                return res.render('index.ejs', {
+                    user: req.session.user,
+                    islogin: req.session.islogin,
+                    // 文章列表
+                    articles: []
+                })
+            }
             res.render('index.ejs', {
                 user: req.session.user,
                 isLogin: req.session.isLogin,
-                article: data[0]
+                articles: data
             })
-        })
+            console.log(data)
 
+        })
     }
 }
